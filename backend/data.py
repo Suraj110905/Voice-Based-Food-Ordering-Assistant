dee19ec347345bd5204f1ae821ddf59a4baa18c5
Suraj Kumar <suraj110905@gmail.com>
2026-03-30 18:03:21 +0530

    Initial commit - Project setup with React frontend and FastAPI backend

---
+++ backend/data.py
@@ -0,0 +1,60 @@
+restaurants = [
+    {
+        "id": 1,
+        "name": "McDonald's",
+        "cuisine": "Fast Food",
+        "rating": 4.2,
+        "menu": [
+            {"item": "McAloo Tikki", "price": 50},
+            {"item": "McChicken Burger", "price": 80},
+            {"item": "French Fries", "price": 40},
+            {"item": "Coke", "price": 30},
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Domino's",
+        "cuisine": "Pizza",
+        "rating": 4.4,
+        "menu": [
+            {"item": "Margherita Pizza", "price": 150},
+            {"item": "Pepperoni Pizza", "price": 200},
+            {"item": "Garlic Bread", "price": 60},
+            {"item": "Coke", "price": 30},
+        ]
+    },
+    {
+        "id": 3,
+        "name": "KFC",
+        "cuisine": "Fast Food",
+        "rating": 4.1,
+        "menu": [
+            {"item": "Chicken Bucket", "price": 350},
+            {"item": "Zinger Burger", "price": 120},
+            {"item": "Coleslaw", "price": 50},
+            {"item": "Pepsi", "price": 30},
+        ]
+    },
+    {
+        "id": 4,
+        "name": "Pizza Hut",
+        "cuisine": "Pizza",
+        "rating": 4.3,
+        "menu": [
+            {"item": "Veggie Supreme Pizza", "price": 180},
+            {"item": "Chicken Supreme Pizza", "price": 220},
+            {"item": "Breadsticks", "price": 70},
+            {"item": "Coke", "price": 30},
+        ]
+    },
+]
+
+# Keywords to match food items from voice input
+food_keywords = {
+    "burger": ["McAloo Tikki", "McChicken Burger", "Zinger Burger"],
+    "pizza": ["Margherita Pizza", "Pepperoni Pizza", "Veggie Supreme Pizza", "Chicken Supreme Pizza"],
+    "fries": ["French Fries"],
+    "chicken": ["Chicken Bucket", "Zinger Burger"],
+    "coke": ["Coke"],
+    "bread": ["Garlic Bread", "Breadsticks"],
+}
